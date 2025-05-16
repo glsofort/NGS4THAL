cb0f12418b4e4dfa14fc8418cc00f5999d88bfaa
namxle <103374580+namxle@users.noreply.github.com>
2025-05-16 10:28:08 +0700

    Update Thalassemia.py

--- nextflow/scripts/Thalassemia.py
+++ nextflow/scripts/Thalassemia.py
@@ -25,21 +25,25 @@ if __name__ == "__main__":
     assembly_version = args.reference
 
     if assembly_version == "hg19":
+        chr16 = "16"
+        chr11 = "11"
         chr16_pos1 = 200000
         chr16_pos2 = 230000
         chr11_pos1 = 5269000
         chr11_pos2 = 5278000
     elif assembly_version == "GRCh38":
+        chr16 = "chr16"
+        chr11 = "chr11"
         chr16_pos1 = 150001
         chr16_pos2 = 180001
         chr11_pos1 = 5247770
         chr11_pos2 = 5256770
 
     alpha_rescue_set = RescueSet(
-        path2bam, "chr16", chr16_pos1, chr16_pos2)
+        path2bam, chr16, chr16_pos1, chr16_pos2)
 
     beta_rescue_set = RescueSet(
-        path2bam, "chr11", chr11_pos1, chr11_pos2)
+        path2bam, chr11, chr11_pos1, chr11_pos2)
 
     hemoglobin_rescue_set = alpha_rescue_set | beta_rescue_set
 
