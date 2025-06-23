1c77395a1943fea069da6997046c2a3582d73324
namxle <103374580+namxle@users.noreply.github.com>
2025-06-23 15:23:27 +0700

    Update conifer.py

--- nextflow/scripts/Conifer/conifer.py
+++ nextflow/scripts/Conifer/conifer.py
@@ -349,7 +349,8 @@ def CF_call(args):
 		mean= np.mean(data.rpkm,axis=1)
 		sd =  np.std(data.rpkm,axis=1)
 
-		print(data)
+		print(mean)
+		print(sd)
 		
 		for sample in r.getSampleList():
 			sample_data = data.getSample([sample]).flatten()
